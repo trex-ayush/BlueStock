28f37200fabf32fa6062f58a1b91af56b209875b
Ayush <singhayush.sa81@gmail.com>
2025-05-02 12:42:45 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ForgotPassword from "./components/ForgotPassword";
 import SharkInvestor from "./components/SharkInvestor";
